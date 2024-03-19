5e78824c16c781efbd49701456116a4ec916134c
subhashis <subhashis.sarangi@gmail.com>
2024-03-19 10:11:32 +0530

    first commit

---
+++ src/main/java/com/myapp/springboot_cicd_2/SpringbootCicd2Application.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot_cicd_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCicd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/springboot_cicd_2/SpringbootCicd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.springboot_cicd_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
